4e132056edf99f14972697976fc6f748f4225901
Semhkaramn <158337999+Semhkaramn@users.noreply.github.com>
2026-05-11 20:33:47 +0300

    Update SponsorsContent.tsx

--- src/components/home/SponsorsContent.tsx
+++ src/components/home/SponsorsContent.tsx
@@ -1,25 +1,8 @@
 'use client'
 
-import { useUserTheme } from '@/components/providers/user-theme-provider'
-import CasinodostlarHome from './themes/CasinodostlarHome'
-import LykibomHome from './themes/LykibomHome'
-import Site1Home from './themes/Site1Home'
-import Site2Home from './themes/Site2Home'
+import UnifiedHome from './UnifiedHome'
 
-// Tema switcher - Aktif temaya göre doğru ana sayfa bileşenini gösterir
+// Tek ana sayfa bileşeni - tüm temalar kaldırıldı
 export default function SponsorsContent() {
-  const { theme } = useUserTheme()
-
-  switch (theme.name) {
-    case 'casinodostlar':
-      return <CasinodostlarHome />
-    case 'lykibom':
-      return <LykibomHome />
-    case 'site1':
-      return <Site1Home />
-    case 'site2':
-      return <Site2Home />
-    default:
-      return <CasinodostlarHome />
-  }
+  return <UnifiedHome />
 }
